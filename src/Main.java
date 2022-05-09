4917f49c778fc195f71abf41308fceb07434da5e
Grecia Alva Bermejo <gz.alvabermejo@gmail.com>
2022-05-09 10:29:29 +0200

    Ejercicios tema 8

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(24);
+        persona.setNombre("Elsa");
+        persona.setTelefono("698453109");
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+
+
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
